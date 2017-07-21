1878fbb354ec8d766a15bd4b40018672b5015187
Simon Friederich <sifriede@uni-mainz.de>
2017-07-21 12:05:53 +0200

    Laser49.ino fixed, now it works

--- laser/Laser48/Laser48.ino
+++ laser/Laser48/Laser48.ino
@@ -1,17 +1,10 @@
-//  Laser controller script
-//  Originally by S.Heidrich
-//  Edited by S.Friederich (IP configuration part)
-//  latest update: 19.07.2017
-const float version = 1.0;
-//========== Needed Packages ==========
-#include <EEPROM.h>
 #include <SPI.h>
 #include <Ethernet2.h> //Maybe this package needs to be changed
 #define MAX_CMD_LENGTH   25
 #include <Wire.h>
 #include <digitalWriteFast.h>
 
-
+byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0x15, 0x89}; // Enter a MAC address
 int wert;
 int binwert;
 int printwert = 0;
@@ -32,41 +25,17 @@ uint32_t value0 = 0;
 const byte interruptPin = 2;
 volatile byte TestVariable = 0;
 boolean connected = false;
-//========== Ethernet ===============
 EthernetServer server(23); 
 EthernetClient client;
 
-//========== Static/DHCP ===============
-byte mac[] = {0x90, 0xA2, 0xDA, 0x10, 0x15, 0x89}; // Enter a MAC address
-
-struct IPObject {
-    boolean dhcp_state; //static IP (false) or DHCP (true)
-    int ip_addr[4]; //IP address
-};
-
-IPObject ip_eeprom;
-int eeAddress = 0; // Defined address for EEPROM
-
-
-//==============================
-
 void setup() { 
-  //===== Serial =====
-    Serial.begin(9600); // Open serial communications and wait for port to open
-
-  //===== Ethernet =====
-    EEPROM.get(eeAddress, ip_eeprom); // Get last written information about DHCP or static IP and save it to ip_eeprom
-    if (ip_eeprom.dhcp_state) 
-        Ethernet.begin(mac); // start the Ethernet connection and the server using DHCP
-    else {
-        byte ip[4];
-        for (int i = 0; i <= 3; i++) ip[i] = ip_eeprom.ip_addr[i];
-        Ethernet.begin(mac, ip); // start the Ethernet connection and the server using static IP written to EEPROM
-    }
-
-    server.begin(); // start server 
-  
-  
+  //TCP:
+  Serial.begin(9600);  
+  Ethernet.begin(mac);  
+  server.begin(); 
+  //Address
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
   //Shift-register:
   pinMode(shiftPin, OUTPUT); //Shift
   pinMode(storePin, OUTPUT); //Store
@@ -88,91 +57,43 @@ void setup() {
 }
 
 void loop() {
-     //===== Ethernet: Create a client connection =====
-    EthernetClient client = server.available();
-    EEPROM.get(eeAddress, ip_eeprom); // Get last written information about DHCP or static IP and save it to ip_eeprom
-
-    //==== Evaluate Serial input ====
-    if (Serial.available() > 0) {
-        String serialstring = Serial.readString(); // read the incoming byte:
-        serialstring.trim(); //removes leading and trailing whitespace
-        Serial.println(serialstring); // write the read String
-
-        if (serialstring.equals("help")) {
-            Serial.println("--- Serial connection Help ---");
-            Serial.println("ip?                 : get current ip address and configuration");
-            Serial.println("set ip 10.32.200.44 : set static ip address");
-            Serial.println("ip: DHCP            : set ip to DHCP");
-        }
-
-        else if (serialstring.equals("ip?")) {
-            Serial.print("server is currently at ");Serial.println(Ethernet.localIP());
-            Serial.print("ip_eeprom.dhcp_state: ");Serial.print(ip_eeprom.dhcp_state);
-            if (ip_eeprom.dhcp_state) 
-                Serial.println("(DHCP)");
-            else {
-                Serial.println("(static)"); Serial.print("EEPROM ip ");
-                for (int i = 0; i <= 2;i++){
-                        Serial.print(ip_eeprom.ip_addr[i]);Serial.print(".");
-                        if (i==2) Serial.println(ip_eeprom.ip_addr[3]);
-                    }
-            }
-        }
-        
-        else if (serialstring.startsWith("set ip ")) {
-            char achar[23]; // Needed to use sscanf with input serialstring
-            int intarray[4]; // Intermediate integer array needed to use sscanf
-            
-            serialstring.toCharArray(achar, sizeof(achar)); // Needed to use sscanf with String a
-                //Error!//sscanf(achar, "set ip %3d.%3d.%3d.%3d", &ip_eeprom.ip_addr[0], &ip_eeprom.ip_addr[1], &ip_eeprom.ip_addr[3], &ip_eeprom.ip_addr[4]); // Write new ip to ip_eeprom //Leads to error!
-            sscanf(achar, "set ip %3d.%3d.%3d.%3d", &intarray[0], &intarray[1], &intarray[2], &intarray[3]); //Assign new ip to intermediate array            Serial.print("read ip: ");
-            for (int i = 0; i <= 3; i++) {
-                ip_eeprom.ip_addr[i] = intarray[i]; // Assign read ip to EEPROM structure
-            }
-            for (int i = 0; i <= 2; i++) {
-                Serial.print(ip_eeprom.ip_addr[i]); Serial.print(".");
-                if (i == 2) {
-                    Serial.print(ip_eeprom.ip_addr[3]); Serial.println();
-                    Serial.println("IP was written to EEPROM (restart necessary)");
+  // Create a client connection
+   EthernetClient client = server.available();
+   if (Serial.available() > 0) {
+                // read the incoming byte:
+               String a = Serial.readString();
+               a.trim();
+               Serial.println(a);
+               if (a.equals("ip?")){
+                  Serial.print("server is at ");
+                  Serial.println(Ethernet.localIP());
                 }
-            }
-            ip_eeprom.dhcp_state = false; // Set IP dhcp_state to 0 for static
-            EEPROM.put(eeAddress, ip_eeprom);
-            }
-            
-        else if (serialstring.equals("ip: DHCP")) {
-            Serial.println("Unset static ip, set DHCP (restart necessary)");
-            ip_eeprom.dhcp_state = true; // Set IP dhcp_state to 1 for DHCP
-            EEPROM.put(eeAddress, ip_eeprom);
-        }
-        
-        else Serial.println("Invalid command, type help");
     }
-    //==== Ethernet ====
-    if (client) {
-        if (!connected) {
-            client.flush(); //Clear out the input buffer
-            cmdstr = ""; //clear the commandString variable
-            connected = true;
-        }
-
-        if (client.available() > 0) {
-            char zeichen = client.read();
-            value0 = strtoul(cmdstr.c_str(), NULL, 10);
-            if (value0 > 0){
-                value = value0;
-            }
-            //convert all capitalized letters into small ones
-            if ((65 <= zeichen) && (zeichen <= 90))
-                zeichen += 32;
-            //Filter all unwanted characters and first character must not be space
-            //only allowed: \n * : . ' ' ? a..z A..Z 0..9
-            if (
-                (zeichen == '\n') || (zeichen == '*') || (zeichen == ':') || (zeichen == '.') || ((zeichen == ' ') && cmdstr.length()) ||
-                (zeichen == '?') || ((48 <= zeichen) && (zeichen <= 57)) || ((97 <= zeichen) && (zeichen <= 122))
-            )
-                readTelnetCommand(zeichen, client);
-        }
+    
+  if (client) {
+    if (!connected) {
+      client.flush(); //Clear out the input buffer
+      cmdstr=""; //clear the commandString variable
+      connected = true;
+      }
+     
+    if (client.available() > 0) {
+      char zeichen = client.read();
+      value0 = strtoul(cmdstr.c_str(), NULL, 10);
+      if (value0 > 0){
+        value = value0;
+      }
+      //convert all capitalized letters into small ones
+      if ((65<= zeichen) && (zeichen <= 90))
+         zeichen += 32;
+      //Filter all unwanted characters and first character must not be space
+      //only allowed: \n * : . ' ' ? a..z A..Z 0..9
+      if (
+        (zeichen == '\n') || (zeichen == '*') || (zeichen == ':') || (zeichen == '.') || ((zeichen == ' ') && cmdstr.length()) ||
+        (zeichen == '?') || ((48<= zeichen) && (zeichen <= 57)) || ((97<= zeichen) && (zeichen <= 122))
+        )
+          readTelnetCommand(zeichen,client);
+      }
     }
   }
 
@@ -203,7 +124,6 @@ void parseCommand(EthernetClient &client) {
   
   //===== HELP =====
   else if(cmdstr.equals("help")) {
-      client.print("--- LASER Controller Version ");client.print(version,1);client.println(" ---");
       client.println("on = value on"); 
       client.println("off = value off");
       client.println("Value between 0 and 65536 = value request");
